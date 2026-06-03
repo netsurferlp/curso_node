fb1c78da599848cf2a34de0e4063e58baac9a62d
gaston <netsurferlp@gamail.com>
2026-06-03 10:30:31 -0300

    actualizacion de auth

--- entrega_proyecto_final/back/src/routes/auth.routes.js
+++ entrega_proyecto_final/back/src/routes/auth.routes.js
@@ -2,8 +2,8 @@ import express from "express";
 const router = express.Router();
 
 //POST /auth/login - Iniciar sesión
-router.post("/login", (req, res) =>
-  res.send("Login exitoso (Bearer token generado)"),
-);
+router.post("/login", (req, res) => {
+  res.send("Login exitoso (Bearer token generado)");
+});
 
 export default router;
