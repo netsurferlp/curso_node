da037da7d9bf5e379c3c4da259f71f5eb7cf9672
netsurferlp <gastonpessolano@gmail.com>
2026-07-10 08:54:33 -0300

    update conf firebase

--- entrega_proyecto_final/back/src/config/firebase.js
+++ entrega_proyecto_final/back/src/config/firebase.js
@@ -10,12 +10,12 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: process.env.apiKey,
-  authDomain: process.env.authDomain,
-  projectId: process.env.projectId,
-  storageBucket: process.env.storageBucket,
-  messagingSenderId: process.env.messagingSenderId,
-  appId: process.env.appId
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId: process.env.APP_ID
 };
 
 // Initialize Firebase
