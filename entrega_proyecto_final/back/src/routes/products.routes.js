cafa4edf2eb73c3e22b23df99a149ddfb29eb60c
gaston <netsurferlp@gamail.com>
2026-07-13 13:30:54 -0300

    Correccion de rutas de productos

--- entrega_proyecto_final/back/src/routes/products.routes.js
+++ entrega_proyecto_final/back/src/routes/products.routes.js
@@ -13,9 +13,9 @@ import { auth } from "../middlewares/auth.middleware.js";
 
 //Prefijo de ruta: /api/
 
-router.get("/", getProducts); // Obtener todos los productos
-router.get("/:id", getProductById); // Obtener un producto por su ID
-router.post("/", auth, createProduct); // Crear un nuevo producto
-router.delete("/:id", auth, deleteProduct); // Eliminar un producto por su ID
+router.get("/products", getProducts); // Obtener todos los productos
+router.get("/products/:id", getProductById); // Obtener un producto por su ID
+router.post("/products/create", auth, createProduct); // Crear un nuevo producto
+router.delete("/products/:id", auth, deleteProduct); // Eliminar un producto por su ID
 
 export default router;
